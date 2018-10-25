8616e06d0e8d7f92d0888205233e4edb7e1d52ef
BranFlakez <BranFlakez@users.noreply.github.com>
2018-10-25 18:47:29 -0400

    might have got the radio working

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-var songs = [
-	[<p>test</p>]
-	[<p><a href="https://www.google.com">test 2</a></p>]
-]
-function newSong() {
-	var randomNumber = Math.floor(Math.random() * (songs.length));
-	document.getElementById('songDisplay').innerHTML = songs[randomNumber];
-}
